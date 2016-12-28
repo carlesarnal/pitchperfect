4176c8668e1373575e5bef34d92c211d72e80ef5
Carles Arnal <carles.arnal@borealos.com>
2016-12-28 15:35:38 +0100

    Initial Commit

---
+++ PerfectPitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfectPitch
+//
+//  Created by Carles Arnal Castelló on 28/12/16.
+//  Copyright © 2016 carlesarnal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
