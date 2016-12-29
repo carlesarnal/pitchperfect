d8d07ae531d7ec938655d65aa99631c2bc31d2f6
Carles Arnal <carles.arnal@borealos.com>
2016-12-29 12:54:12 +0100

    Solved problems with landscape

--- PerfectPitch/PlaySoundsViewController.swift
+++ PerfectPitch/PlaySoundsViewController.swift
@@ -38,6 +38,15 @@ class PlaySoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        snailButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        chipmunkButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        rabbitButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        vaderButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        echoButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        reverbButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        stopButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+
+
         setupAudio()
         // Do any additional setup after loading the view.
     }
